d6e4de1a0eae022a747ecdb0f5988fae5832db82
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-12-13 17:29:24 -0500

    Update formatting

--- src/lib/slack.ts
+++ src/lib/slack.ts
@@ -67,7 +67,6 @@ export async function setPresenceMode(active: boolean): Promise<Response> {
     "token",
     Deno.env.get("SLACK_TOKEN")!,
   );
-
   formData.append(
     "presence",
     active ? "active" : "away",
